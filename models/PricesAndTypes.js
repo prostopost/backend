cef6bfb94e9c4af0a6230f05cdc7c4cca261223a
daniiltserin <daniilborowkow@yandex.ru>
2017-01-04 22:02:33 +0600

    Created base model for Prices and Types

---
+++ models/PricesAndTypes.js
@@ -0,0 +1,28 @@
+var mongoose = require("mongoose");
+
+var patSchema = {
+    _id: ObjectId,
+    name: String,
+    description: String,
+    price: String
+
+};
+
+/**
+ * Цены и типы услуг
+ */
+var PAT = module.exports = mongoose.model("PAT", patSchema);
+
+/**
+ * Создать новый тип
+ */
+module.exports.newPAT = function (newUser, callback) {
+    newUser.save(callback);
+};
+
+/**
+ * Находим услугу
+ */
+module.exports.findPAT = function (query, callback) {
+    Courier.find(query, callback);
+};
