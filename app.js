dec2cddfed3b8e3cd9a45a4bf7cf1e6a5d9b3ddb
daniiltserin <daniilborowkow@yandex.ru>
2017-01-04 19:15:00 +0600

    Created Courier model

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var config = require('./config');
 
 var app = express();
+mongoose.connect(config.mongo.connectString);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,7 +29,7 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -37,7 +40,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -48,12 +51,16 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
     error: {}
   });
+});
+
+app.listen(3000, function () {
+  console.log("Launched");
 });
 
 
---
+++ models/Courier.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+
+var courierSchema = mongoose.Schema({
+    name: String,   //courier name
+    lastname: String,   //courier last name
+    regDate: {  //registration date.
+        type: Date,
+        default: Date.now
+    },
+    autoType: Number,   //one of many autotypes
+    auto: String,   //full auto description(he fill its self)
+    autoId: Number, //auto id from autos models
+    isFree: Boolean,    //if courier is full free, he can take hot orders
+    license: String,    //pay tax?
+    birthDay: Number,   //gratuliations programm is cool
+    specs: Array    //orders type loved by courier
+});
+
+var Courier = module.exports = mongoose.model("Courier", courierSchema);
+
+module.exports.registerCourier = function (newCourier, callback) {
+    newCourier.save(callback);
+};
+
+module.exports.findCouriers = function (query, callback) {
+    Courier.find(query, callback);
+};
